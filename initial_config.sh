9c91c742c5476a98bf2529af2ed569feaad6d701
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-05-17 21:41:34 -0700

    added commands to install docker

--- initial_config.sh
+++ initial_config.sh
@@ -31,4 +31,11 @@ synclient HorizTwoFingerScroll=1
 synclient PalmDetect=1
 #sound settings 
 amixer -D pulse sset Master 60%
-
+#install docker
+apt-get update
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+touch /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee docker.list
+apt-get install linux-image-extra-$(uname -r)
+apt-get install -y docker-engine
